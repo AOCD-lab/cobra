a83c41120d6e54af14d633fc267dcce088af4ca5
Luigi Cavallo <luigi.cavallo@kaust.edu.sa>
2023-10-19 22:03:48 +0200

    1st commit

---
+++ Fortran/dnormal1.f
@@ -0,0 +1,30 @@
+!========================================================================= ! Subroutine dnorm
+!=========================================================================
+      SUBROUTINE dnormal(vec, n, ind, dm, sd)
+      Implicit None
+
+      Integer :: n
+      Integer :: ind
+      Integer :: i
+
+      Real*8 :: vec(n)      ! set of variables
+      Real*8 :: dm          ! mean value
+      Real*8 :: sd          ! standard deviation
+
+      dm = 0.0
+      do i = 1, n
+        if(i.ne.ind) dm = dm + vec(i)
+      enddo
+      dm = dm / dble(n)
+
+      sd = 0.0
+      do i = 1, n
+        if(i.ne.ind) sd = sd + (vec(i) - dm)**2.0
+      enddo
+      sd = dsqrt (sd / dble(n-1))    
+
+      do i = 1, n
+        if(i.ne.ind) vec(i) = (vec(i) - dm)/sd
+      enddo
+
+      END
