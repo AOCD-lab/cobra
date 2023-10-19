493296429b03b3e2cd7ea0deb1e87a799a57f1c4
Luigi Cavallo <luigi.cavallo@kaust.edu.sa>
2023-10-19 22:03:48 +0200

    1st commit

---
+++ Utilities/set_variables.py
@@ -0,0 +1,7 @@
+def GetVariables():
+
+    PYTHONHOME = "/Users/cavalll/Desktop/MLR-Vbur/Sources/Distribution/New/" 
+    MLRBinary =  PYTHONHOME+"/Fortran/MLR.x" 
+
+    return MLRBinary, PYTHONHOME
+
