57e7ff83eb29437772341d794d0c83d7a9ba9dc2
Luigi Cavallo <luigi.cavallo@kaust.edu.sa>
2023-12-09 18:06:42 +0100

    plot delta

--- Plots/plot_radar.py
+++ Plots/plot_radar.py
@@ -72,45 +72,53 @@ fig, ax = plt.subplots(figsize=(5,5), subplot_kw=dict(polar=True))
 
 basename = os.path.splitext(sys.argv[1])[0]
 
-ax.plot(label_loc, data1+data3, lw=2, label='reference')
-ax.plot(label_loc, data1, lw=3, label=basename)
+ax.plot(label_loc, data3, lw=3, label=basename, zorder=2)
 
 ax.set_theta_direction(-1)
 ax.set_theta_offset(np.pi / 2.0)
 
 ax.spines['polar'].set_linewidth(3)
-    
 ax.grid(color='black', alpha=1.0)
-    
-ax.set_ylim(0, 1.2)
+
+
+# find plot limits
+
+ymin = np.min(data3)
+ymax = np.max(data3)
+
+test = np.arange(-2.0,+2.0,0.2)
+
+for it in range(len(test)):
+    if test[it] > ymax:
+       ymax = test[it]
+       break
+    if test[it] > ymin:
+       ymin = test[it-1]
+       break
+
+ax.set_ylim(ymin,ymax)
 
 ax.tick_params(axis='both', which='major', pad=20, labelsize=12)
-
-#ax.yaxis.set_major_locator(MultipleLocator(1))
-#ax.yaxis.set_minor_locator(AutoMinorLocator(5))
-    
 ax.legend(loc="lower right", bbox_to_anchor=(.1, 1.0))
 
 ax.yaxis.grid(True)
-
 ax.grid(which='minor', color='#EEEEEE', linestyle=':', linewidth=0.5)
-
+ax.yaxis.set_major_locator(ticker.FixedLocator([-1.2,-1.0,-0.8,-0.6,-0.4,-0.2,0.0,0.2,0.4,0.6,0.8,1.0,1.2]))
+ax.yaxis.set_minor_locator(ticker.FixedLocator([0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.9, 1.1]))
 ax.minorticks_on()
 
-ax.yaxis.set_major_locator(ticker.FixedLocator([0.2, 0.4, 0.6, 0.8, 1.0, 1.2]))
-ax.yaxis.set_minor_locator(ticker.FixedLocator([0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.9, 1.1]))
 
-#ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.2))
+# plot zero axis in red color
+
+circle = plt.Circle((0, 0), 0.6, transform=ax.transData._b, fill=False, edgecolor='red', linewidth=2, zorder=1)
+plt.gca().add_artist(circle)
+
 
 ax.set_rlabel_position(180/1 + 180/12)
-
-#ax.set_yticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0])
 
 lines, labels = plt.thetagrids(np.degrees(label_loc), labels=label)
 
 plt.tight_layout()
-
-#plt.show()
 
 filename, file_extension = os.path.splitext(sys.argv[1])
 plt.savefig(filename+"-pcs.ps")
