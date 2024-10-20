47522158565b8a9bc4f71ba1ca05607c33189387
kawallo <luigi.cavallo@kaust.edu.sa>
2024-10-20 18:40:06 +0300

    revision 1

---
+++ Examples/runall.sh
@@ -0,0 +1,13 @@
+
+
+for M in LQ-example HQ-example case-01 case-02 case-03 case-04 case-05 case-06 case-07 01 02 03 04 05
+do
+    cd $M
+
+    python3 ../../runtests.py -m $M.matrix -d up -n 1000 -t .2 -b .1 -o .2 -c 0.8
+    python3 ../../runplots.py -m $M.matrix 
+
+    cd ..
+done
+
+
