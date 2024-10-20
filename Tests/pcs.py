b8fbf454157a788b0f94fc0fa3932ac30fd84ecb
kawallo <luigi.cavallo@kaust.edu.sa>
2024-10-20 18:05:38 +0300

    revision 1

--- Tests/pcs.py
+++ Tests/pcs.py
@@ -2,18 +2,16 @@
 
 """ ---------------------------------------------------------------------------
 
-This script runs Leave one out cross validation and calculate Q^2
+This script assembles results from all tests and write a file for the radar plot
 
 To be run as:
-   python runloo.py -m sys08.matrix
+   python pcs.py -m myfile.matrix
 
 Input:
    MatrixFile : = myfile.matrix       Matrix describing the dataset to be learned
 
 Output:
-   LOOFile  : = myfile.loo          Output file with experimental and LOOCV values
-   Q2File   : = myfile.q2           Output file having inside R2 
-   MAEFile  : = myfile.mae_loo      Output file having inside MAE from LOOCV
+   OUTFile  : = myfile.pcs          Output file with summary of all data
 
 @author: Zhen Cao, Luigi Cavallo
 --------------------------------------------------------------------------- """
@@ -32,9 +30,7 @@ def GetFiles():
     """ ----------------------------------------------------
     Handles input/output and excutable files
 
-    MatrixFile  := matrix file name, i.e., sys08.matrix
-    LOOFile     := stores experimental and LOO values
-    Q2File      := stores Q2 
+    MatrixFile  := matrix file name, i.e., myfile.matrix
 
     ---------------------------------------------------- """
 
@@ -46,7 +42,7 @@ def GetFiles():
     args       = parser.parse_args()
 
     if (not args.matrix) :
-       print(' Usage:  run_loo.py -m/--matrix file.matrix')
+       print(' Usage:  pcs.py -m/--matrix file.matrix')
        exit()
 
 
@@ -154,60 +150,40 @@ def main():
     cycle_rec = words[2]
 
 
-# calculate PCS
+# write data for radar plot. Manipulate some metrics to have 1.0 as ideal behavior
 
     PCS = np.zeros(13)
 
-
-  # last reference values
-
-    aR2      =  0.903
-    MAE_Fit  =  0.198
-    Q2       =  0.852
-    MAE_LOO  =  0.289
-    aYR2_Max =  0.817
-    aYR2_Ave =  0.279
-    MAE_PR   =  0.179
-    MAE_CF   =  0.170
-    MAE_T20  =  0.403
-    Acc_80   =  0.743
-    Pre_80   =  0.762
-    Rec_80   =  0.776
-
-
-    PCS[1]  =   (mlr_ar2        - aR2      )
-    PCS[2]  =  -(mlr_mae        - MAE_Fit  )
-    PCS[3]  =   (loo_q2         - Q2       )
-    PCS[4]  =  -(loo_mae        - MAE_LOO  )
-    PCS[5]  =  -(yrand_syr2_max - aYR2_Max )
-    PCS[6]  =  -(yrand_syr2_ave - aYR2_Ave )
-    PCS[7]  =  -(boot_mae_fit   - MAE_PR   )
-    PCS[8]  =  -(boot_mae_cff   - MAE_CF   )
-    PCS[9]  =  -(pred_mae       - MAE_T20  )
-    PCS[10] =   (cycle_pre      - Acc_80   )
-    PCS[11] =   (cycle_acc      - Pre_80   )
-    PCS[12] =   (cycle_rec      - Rec_80   )
-
-    PCS[0] = np.sum(PCS[1:13])
-    PCS[0] = PCS[0]/12.0
+    PCS[0]  =       mlr_ar2        
+    PCS[1]  =  1.0 -mlr_mae        
+    PCS[2]  =       loo_q2         
+    PCS[3]  =  1.0 -loo_mae        
+    PCS[4]  =  1.0 -yrand_syr2_max 
+    PCS[5]  =  1.0 -yrand_syr2_ave 
+    PCS[6]  =  1.0 -boot_mae_fit   
+    PCS[7]  =  1.0 -boot_mae_cff   
+    PCS[8]  =  1.0 -pred_mae       
+    PCS[9]  =       cycle_pre      
+    PCS[10] =       cycle_acc      
+    PCS[11] =       cycle_rec      
 
     with open(OUTFile, "w") as f:
-         f.write("{:10s}".format('             ') + "{:10s}".format('Reference') + "{:10s}".format(Basename       ) + "{:10s}".format('Delta ') + '\n' )
-         f.write("{:10s}".format('$R^2$        ') + "{:8.3f}".format(aR2     )   + "{:8.3f}".format(mlr_ar2       ) + "{:8.3f}".format(PCS[1])  + '\n' )
-         f.write("{:10s}".format('$MAE_{Fit}$  ') + "{:8.3f}".format(MAE_Fit )   + "{:8.3f}".format(mlr_mae       ) + "{:8.3f}".format(PCS[2])  + '\n' )
-         f.write("{:10s}".format('$Q^2$        ') + "{:8.3f}".format(Q2      )   + "{:8.3f}".format(loo_q2        ) + "{:8.3f}".format(PCS[3])  + '\n' )
-         f.write("{:10s}".format('$MAE_{LOO}$  ') + "{:8.3f}".format(MAE_LOO )   + "{:8.3f}".format(loo_mae       ) + "{:8.3f}".format(PCS[4])  + '\n' )
-         f.write("{:10s}".format('$SR^2_{Max}}$') + "{:8.3f}".format(aYR2_Max)   + "{:8.3f}".format(yrand_syr2_max) + "{:8.3f}".format(PCS[5])  + '\n' )
-         f.write("{:10s}".format('$SR^2_{Av}$  ') + "{:8.3f}".format(aYR2_Ave)   + "{:8.3f}".format(yrand_syr2_ave) + "{:8.3f}".format(PCS[6])  + '\n' )
-         f.write("{:10s}".format('$MAD_{PR}$   ') + "{:8.3f}".format(MAE_PR  )   + "{:8.3f}".format(boot_mae_fit  ) + "{:8.3f}".format(PCS[7])  + '\n' )
-         f.write("{:10s}".format('$MAD_{CF}$   ') + "{:8.3f}".format(MAE_CF  )   + "{:8.3f}".format(boot_mae_cff  ) + "{:8.3f}".format(PCS[8])  + '\n' )
-         f.write("{:10s}".format('$MAE_{T20}$  ') + "{:8.3f}".format(MAE_T20 )   + "{:8.3f}".format(pred_mae      ) + "{:8.3f}".format(PCS[9])  + '\n' )
-         f.write("{:10s}".format('$Acc_{80}$   ') + "{:8.3f}".format(Acc_80  )   + "{:8.3f}".format(cycle_pre     ) + "{:8.3f}".format(PCS[10]) + '\n' )
-         f.write("{:10s}".format('$Pre_{80}$   ') + "{:8.3f}".format(Pre_80  )   + "{:8.3f}".format(cycle_acc     ) + "{:8.3f}".format(PCS[11]) + '\n' )
-         f.write("{:10s}".format('$Rec_{80}$   ') + "{:8.3f}".format(Rec_80  )   + "{:8.3f}".format(cycle_rec     ) + "{:8.3f}".format(PCS[12]) + '\n' )
-         f.write("{:10s}".format('PCS     ')      + "{:8.3f}".format(PCS[0])     + '\n' )
+         f.write("{:10s}".format('             ') + "{:10s}".format(Basename) + '\n' )
+         f.write("{:10s}".format('$R^2$        ') + "{:8.3f}".format(PCS[0])  + '\n' )
+         f.write("{:10s}".format('$MAE_{Fit}$  ') + "{:8.3f}".format(PCS[1])  + '\n' )
+         f.write("{:10s}".format('$Q^2$        ') + "{:8.3f}".format(PCS[2])  + '\n' )
+         f.write("{:10s}".format('$MAE_{LOO}$  ') + "{:8.3f}".format(PCS[3])  + '\n' )
+         f.write("{:10s}".format('$SR^2_{Max}}$') + "{:8.3f}".format(PCS[4])  + '\n' )
+         f.write("{:10s}".format('$SR^2_{Av}$  ') + "{:8.3f}".format(PCS[5])  + '\n' )
+         f.write("{:10s}".format('$MAD_{BP}$   ') + "{:8.3f}".format(PCS[6])  + '\n' )
+         f.write("{:10s}".format('$MAD_{BC}$   ') + "{:8.3f}".format(PCS[7])  + '\n' )
+         f.write("{:10s}".format('$MAE_{T20}$  ') + "{:8.3f}".format(PCS[8])  + '\n' )
+         f.write("{:10s}".format('$Acc_{80}$   ') + "{:8.3f}".format(PCS[9])  + '\n' )
+         f.write("{:10s}".format('$Pre_{80}$   ') + "{:8.3f}".format(PCS[10]) + '\n' )
+         f.write("{:10s}".format('$Rec_{80}$   ') + "{:8.3f}".format(PCS[11]) + '\n' )
 
 
 #==============================================================================
 if __name__ == '__main__':
      main()
+
